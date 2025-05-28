2640757a71e0c0fc1af3fdf48cd266764228479a
TrieuDang <dangbinhtrieu123@gmail.com>
2025-05-29 02:54:16 +0700

    update config docker file

--- db/data-source.ts
+++ db/data-source.ts
@@ -7,8 +7,8 @@ config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.PORT) || 5432,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 6543,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
